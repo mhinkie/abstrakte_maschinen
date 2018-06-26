89cb26895f1d80bb1feaa848da4956c9dffb8bbb
alex <e1429186@student.tuwien.ac.at>
2018-06-26 14:12:28 +0200

    removed duplicate test files

--- v2/compiler_reg/util.go
+++
@@ -1,9 +0,0 @@
-/* Utility funktionen zum arbeiten mit goyacc und nex */
-
-package main
-
-/* Stellt die Text-Funktion vom darunterliegenden lexer zur verfügung */
-func yyText(yylex yyLexer) string {
-	l := (*Lexer)(yylex.(*Lexer))
-	return l.Text()
-}
